4d621fac2f3124e10355244e45c94af25ab7bbbd
Ariel Tynan <90798242+arieltynan@users.noreply.github.com>
2022-01-23 15:07:35 -0700

    Add files via upload

---
+++ Problem_006.py
@@ -0,0 +1,18 @@
+# Sum square difference
+
+import math
+
+i = 1
+sumsquare = 0
+totsum = 0
+while i < 101:
+    sumsquare = sumsquare + i*i 
+    totsum = totsum + i
+    i = i + 1
+
+squaresum = totsum*totsum
+
+dif = squaresum - sumsquare
+print(squaresum)
+print(sumsquare)
+print(dif)
